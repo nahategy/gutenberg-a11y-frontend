33194d9d9a28f026f8e0dab2af64ff6804f3875d
mi <nahategy@gmail.com>
2020-10-09 14:53:33 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+import './style.css';
+import proba from "./module/test";
+
+function component() {
+    console.log("asd")
+}
+
+document.body.appendChild(component());
---
+++ src/module/test.js
@@ -0,0 +1,5 @@
+function proba() {
+    alert("asd");
+}
+
+export default proba;
