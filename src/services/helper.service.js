2dbb9a2d05f1dfa20d4fdb70a1cbee965316a799
mi <nahategy@gmail.com>
2020-11-12 20:52:29 +0100

    add: listening to dom changes in the editor

--- src/services/helper.service.js
+++ src/services/helper.service.js
@@ -1,4 +1,5 @@
 import get_language_file from "./language.service";
+import ContrastRule from "../rules/ContrastRule";
 
 class HelperService {
     static language;
@@ -15,7 +16,25 @@ class HelperService {
 
     static initializeAccessibilityChecker() {
         HelperService.language = get_language_file()
+        let rule = new ContrastRule();
+        rule.run();
+        console.log(rule.error_description);
+        HelperService.initializeLib()
 
+    }
+
+    static initializeLib() {
+        let node = document.getElementsByClassName('block-editor-block-list__layout is-root-container')[0];
+        if (!node) {
+            setTimeout(HelperService.initializeLib, 500);
+            return
+        }
+        const config = {attributes: true, childList: true, subtree: true};
+        const observer = new MutationObserver(function (mutationlist, observeer) {
+            console.log(mutationlist);
+            console.log(observeer);
+        });
+        observer.observe(node, config)
     }
 
 }
