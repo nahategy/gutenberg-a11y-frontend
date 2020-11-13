ed4cbb977634c04da38a8528bbf8b0d056a73b48
mi <nahategy@gmail.com>
2020-11-13 19:52:53 +0100

    add: Rule for contrast (only  frame)

---
+++ src/rules/ContrastRule.js
@@ -0,0 +1,14 @@
+import ARule from "./AbstractRule";
+
+
+class ContrastRule extends ARule {
+    error_description = "asd"
+    run() {
+            console.log('contrast rule')
+    }
+}
+
+
+export default ContrastRule;
+
+
