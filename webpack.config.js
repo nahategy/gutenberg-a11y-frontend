97f0af3afccae032b650cde94fabf5df16b436e7
mi <mi@mi-tech.hu>
2021-03-15 16:54:57 +0100

    add: automatic copy sh after rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,48 @@
 const path = require('path');
+const exec = require('child_process').exec;
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'instance.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        contentBase: './dist',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'instance.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          'style-loader',
+          'css-loader',
         ],
-    },
-    resolve: {
-        extensions: ['*', '.js']
-    },
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  plugins: [
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+          exec('./copy.sh', (err, stdout, stderr) => {
+            if (stdout) process.stdout.write(stdout);
+            if (stderr) process.stderr.write(stderr);
+          });
+        });
+      }
+    }
+  ]
+
 };
+
