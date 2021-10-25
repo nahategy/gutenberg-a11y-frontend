13ec8c5f737d15d9b67eb27a677d85df1a1154c6
sSharkyY <ncoreus3r@gmail.com>
2021-10-25 19:19:09 +0200

    empty button

---
+++ src/rules/EmptyButton.js
@@ -0,0 +1,146 @@
+import ARule from "./AbstractRule";
+
+
+class EmptyButton extends ARule {
+    error_description = "Buttons should not be empty!"
+    name = "Empty Button";
+    link = "https://www.w3.org/TR/WCAG20-TECHS/H37.html";
+
+    nextButtonRule;
+    prevButton;
+    currentNumber = 0;
+    currentFaliedNumber = 0;
+    errors = [];
+    errors_element;
+    errornumbersContainerRule;
+    rulename;
+    form_container;
+
+    prevButton;
+    nextButton;
+    repairButton;
+    alt_tag;
+
+
+    prev_rule(ev) {
+        ev.preventDefault();
+        if (this.currentNumber - 1 >= 0) {
+            this.currentFaliedNumber = this.fails.length;
+            this.currentNumber--;
+            this.errornumbersContainerRule.innerHTML = `${this.currentNumber + 1} / ${this.currentFaliedNumber}`;
+            this.alt_tag.value = this.fails[this.currentNumber].alt;
+            this.showFailedElementInDom();
+        }
+
+    }
+
+    next_rule(ev) {
+        ev.preventDefault();
+        if (this.currentNumber + 1 < this.fails.length) {
+            this.currentFaliedNumber = this.fails.length;
+            this.currentNumber++;
+            this.errornumbersContainerRule.innerHTML = `${this.currentNumber + 1} / ${this.currentFaliedNumber}`;
+            this.alt_tag.value = this.fails[this.currentNumber].alt;
+            this.showFailedElementInDom();
+        }
+
+    }
+
+    showFailedElementInDom = () => {
+    }
+
+    repair(ev) {
+        ev.preventDefault();
+        this.fails[this.currentNumber].innerText = this.alt_tag.value;
+        console.log(this.fails[this.currentNumber])
+        this.showAlert('Error corrected', 'alert-primary');
+        this._update();
+    }
+
+    check_element = (element) => {
+        let e = element;
+        let $element = jQuery(e);
+
+        if (
+            (
+                $element.is('button')
+                || $element.is('submit')
+                || $element.is('input[type="button"]')
+                || $element.is('input[type="submit"]')
+            ) && $element.text().trim() === "") {
+            this.fails.push(e);
+        }
+        for (let i = 0; i < $element.children().length; i++) {
+            this.check_element($element.children()[i])
+        }
+    }
+
+    _run = () => {
+        if (!this.block_content)
+            return;
+
+        for (let i = 0; i < this.block_content.length; i++) {
+            this.check_element(this.block_content[i]);
+        }
+
+        return this.fails.length < 1;
+    }
+
+    form() {
+        this.currentNumber = 0;
+        this.currentFaliedNumber = 0;
+        this.currentFaliedNumber = this.fails.length;
+        var current_error = this.fails[0];
+        for (var i = 0; i < this.fails.length; i++) {
+            console.log('alt ', i, ' ', this.fails[i].alt);
+        }
+
+        setTimeout(() => {
+            this.showFailedElementInDom();
+        }, 500);
+
+        var div = document.createElement("div");
+        div.setAttribute("class", "repair_div");
+        div.setAttribute("id", "repair_div");
+        div.classList.add("container");
+        var element = document.createElement("h4");
+        element.innerHTML = "Current error fix:";
+        div.appendChild(element);
+        element = document.createElement("label");
+        element.innerHTML = "Issue   ";
+        this.errornumbersContainerRule = document.createElement("span");
+        element.appendChild(this.errornumbersContainerRule);
+        this.errornumbersContainerRule.innerHTML = (this.currentNumber + 1) + " / " + (this.currentFaliedNumber);
+        div.appendChild(element);
+        this.alt_tag = document.createElement("input");
+        this.alt_tag.classList.add = "alt_tag";
+        this.alt_tag.className = "alt_tag";
+        this.alt_tag.type = "text";
+        div.appendChild(this.alt_tag);
+        var button_container_rule = document.createElement('div');
+        button_container_rule.classList.add('button-container-rule');
+        this.repairButton = document.createElement("button");
+        this.repairButton.innerHTML = "Repair";
+        this.repairButton.classList.add("repair");
+        this.repairButton.onclick = this.repair.bind(this);
+        button_container_rule.appendChild(this.repairButton);
+        this.prevButton = document.createElement("button");
+        this.prevButton.innerHTML = "Prev";
+        this.prevButton.classList.add("prev_rule_error");
+        this.prevButton.onclick = this.prev_rule.bind(this);
+        button_container_rule.appendChild(this.prevButton);
+        this.nextButtonRule = document.createElement("button");
+        this.nextButtonRule.innerHTML = "Next";
+        this.nextButtonRule.classList.add("next_rule_error");
+        this.nextButtonRule.onclick = this.next_rule.bind(this);
+        button_container_rule.appendChild(this.nextButtonRule);
+        div.appendChild(button_container_rule);
+        console.log(div);
+        return div;
+    }
+}
+
+export default EmptyButton;
+
+
+
